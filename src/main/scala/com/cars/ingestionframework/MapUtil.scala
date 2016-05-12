7a329e55be2b134d8c17135c52a4e04cdee4def8
Michael Sesterhenn <invadergir@users.noreply.github.com>
2016-05-11 21:17:32 -0500

    Easier merge

--- src/main/scala/com/cars/ingestionframework/MapUtil.scala
+++ src/main/scala/com/cars/ingestionframework/MapUtil.scala
@@ -6,17 +6,20 @@ import scala.annotation.tailrec
 object MapUtil
 {
   /** Merge two maps.  There must be an easier way to do this. (TODO)
+    * NOTE that the "additionMap" is merged into the existingMap. 
+    * Any existing keys are overwritten.
     */
   def merge(additionMap: StringMap, existingMap: StringMap): StringMap = {
-    @tailrec
-    def recursiveMerge(remainingMap: StringMap, accumulatedMap: StringMap): StringMap = {
-      val headOpt = remainingMap.headOption
-      if(headOpt.isEmpty)
-        accumulatedMap
-      else 
-        recursiveMerge(remainingMap.tail, accumulatedMap + headOpt.get)
-    }
-    recursiveMerge(additionMap, existingMap)
+    //@tailrec
+    //def recursiveMerge(remainingMap: StringMap, accumulatedMap: StringMap): StringMap = {
+    //  val headOpt = remainingMap.headOption
+    //  if(headOpt.isEmpty)
+    //    accumulatedMap
+    //  else 
+    //    recursiveMerge(remainingMap.tail, accumulatedMap + headOpt.get)
+    //}
+    //recursiveMerge(additionMap, existingMap)
+    existingMap ++ additionMap // there you go, there's the easier way
   }
 }
 

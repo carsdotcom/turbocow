9b9001eeb0850aba9caf2f30b01db7c3f6f00fc2
Michael Sesterhenn <invadergir@users.noreply.github.com>
2016-05-11 23:23:21 -0500

    Refactored test replace-null-with-0, so we could add test replace-null-with-999 without duplicating code.

--- src/test/scala/com/cars/ingestion-framework/specs/ExampleAppSpec.scala
+++ src/test/scala/com/cars/ingestion-framework/specs/ExampleAppSpec.scala
@@ -65,8 +65,9 @@ class ExampleAppSpec extends UnitSpec {
       }
     }
 
-    it("should successfully process replace-null-with-0") {
-
+    /** Helper test function
+      */
+    def testReplaceNullWith(value: Int) = {
       // initialise spark context
       val conf = new SparkConf().setAppName("ExampleApp").setMaster("local[1]")
       val sc = new SparkContext(conf)
@@ -74,19 +75,28 @@ class ExampleAppSpec extends UnitSpec {
       try {
         val enriched: List[Map[String, String]] = ExampleApp.enrich(
           sc, 
-          configFilePath = "./src/test/resources/testconfig-integration-replacenullwith.json", 
+          configFilePath = s"./src/test/resources/testconfig-integration-replacenullwith${value.toString}.json", 
           inputFilePath = "./src/test/resources/input-integration-replacenullwith.json")
 
         enriched.size should be (1) // always one because there's only one json input object
         //println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX enriched = "+enriched)
         enriched.head("AField") should be ("A")
-        enriched.head("CField") should be ("0") // this one was null
-        enriched.head("DField") should be ("0") // this one was missing
+        enriched.head("CField") should be (value.toString) // this one was null
+        enriched.head("DField") should be (value.toString) // this one was missing
       }
       finally {
         // terminate spark context
         sc.stop()
       }
+    }
+
+    it("should successfully process replace-null-with-0") {
+      testReplaceNullWith(0)
+    }
+
+    // make sure any value will work
+    it("should successfully process replace-null-with-999") {
+      testReplaceNullWith(999)
     }
 
   }

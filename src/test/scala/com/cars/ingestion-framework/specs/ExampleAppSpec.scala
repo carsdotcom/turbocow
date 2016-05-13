9271db1dc0b811bebba94f60f4992a01244888fa
Michael Sesterhenn <invadergir@users.noreply.github.com>
2016-05-13 13:15:04 -0500

    Use one spark context for all tests.

--- src/test/scala/com/cars/ingestion-framework/specs/ExampleAppSpec.scala
+++ src/test/scala/com/cars/ingestion-framework/specs/ExampleAppSpec.scala
@@ -13,6 +13,11 @@ import org.apache.spark.SparkConf
 //@org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
 class ExampleAppSpec extends UnitSpec {
 
+  // initialise spark context
+  //val conf = new SparkConf().setAppName("ExampleApp").setMaster("local[1]")
+  val conf = new SparkConf().setAppName("ExampleApp").setMaster("local[2]")
+  val sc = new SparkContext(conf)
+      
   // before all tests have run
   override def beforeAll() = {
     super.beforeAll()
@@ -32,6 +37,9 @@ class ExampleAppSpec extends UnitSpec {
   // after all tests have run
   override def afterAll() = {
     super.afterAll()
+
+    // stop spark
+    sc.stop()
   }
 
   //////////////////////////////////////////////////////////////////////////////
@@ -44,50 +52,30 @@ class ExampleAppSpec extends UnitSpec {
   {
     it("should successfully process simple-copy") {
     
-      // initialise spark context
-      val conf = new SparkConf().setAppName("ExampleApp").setMaster("local[1]")
-      val sc = new SparkContext(conf)
-      
-      try {
-        val enriched: List[Map[String, String]] = ExampleApp.enrich(
-          sc, 
-          configFilePath = "./src/test/resources/testconfig-integration-simplecopy.json", 
-          inputFilePath = "./src/test/resources/input-integration.json")
-    
-        enriched.size should be (1) // always one because there's only one json input object
-        //println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX enriched = "+enriched)
-        enriched.head("AField") should be ("A")
-        enriched.head("CField") should be ("10")
-      }
-      finally {
-        // terminate spark context
-        sc.stop()
-      }
+      val enriched: List[Map[String, String]] = ExampleApp.enrich(
+        sc, 
+        configFilePath = "./src/test/resources/testconfig-integration-simplecopy.json", 
+        inputFilePath = "./src/test/resources/input-integration.json")
+  
+      enriched.size should be (1) // always one because there's only one json input object
+      //println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX enriched = "+enriched)
+      enriched.head("AField") should be ("A")
+      enriched.head("CField") should be ("10")
     }
 
     /** Helper test function
       */
     def testReplaceNullWith(value: Int) = {
-      // initialise spark context
-      val conf = new SparkConf().setAppName("ExampleApp").setMaster("local[1]")
-      val sc = new SparkContext(conf)
-      
-      try {
-        val enriched: List[Map[String, String]] = ExampleApp.enrich(
-          sc, 
-          configFilePath = s"./src/test/resources/testconfig-integration-replacenullwith${value.toString}.json", 
-          inputFilePath = "./src/test/resources/input-integration-replacenullwith.json")
+      val enriched: List[Map[String, String]] = ExampleApp.enrich(
+        sc, 
+        configFilePath = s"./src/test/resources/testconfig-integration-replacenullwith${value.toString}.json", 
+        inputFilePath = "./src/test/resources/input-integration-replacenullwith.json")
 
-        enriched.size should be (1) // always one because there's only one json input object
-        //println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX enriched = "+enriched)
-        enriched.head("AField") should be ("A")
-        enriched.head("CField") should be (value.toString) // this one was null
-        enriched.head("DField") should be (value.toString) // this one was missing
-      }
-      finally {
-        // terminate spark context
-        sc.stop()
-      }
+      enriched.size should be (1) // always one because there's only one json input object
+      //println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX enriched = "+enriched)
+      enriched.head("AField") should be ("A")
+      enriched.head("CField") should be (value.toString) // this one was null
+      enriched.head("DField") should be (value.toString) // this one was missing
     }
 
     it("should successfully process replace-null-with-0") {
@@ -99,9 +87,22 @@ class ExampleAppSpec extends UnitSpec {
       testReplaceNullWith(999)
     }
 
+/*
+    it("should successfully process a lookup action") {
+      val enriched: List[Map[String, String]] = ExampleApp.enrich(
+        sc, 
+        configFilePath = "./src/test/resources/testconfig-integration-lookup.json", 
+        inputFilePath = "./src/test/resources/input-integration.json")
+  
+      enriched.size should be (1) // always one because there's only one json input object
+      //println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX enriched = "+enriched)
+      enriched.head("EnhField1") should be ("1")
+      enriched.head("EnhField2") should be ("2")
+      enriched.head("EnhField3") should be ("3")
+    }
+*/
+
   }
 }
 
-
-
-
+  

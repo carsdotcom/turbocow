4e365b5f30b4d5ea47e263a68e453cd3457c7589
Michael Sesterhenn <invadergir@users.noreply.github.com>
2016-05-19 17:51:36 -0500

    Fixed imports

--- src/main/scala/com/cars/ingestionframework/exampleapp/ExampleCustomActionCreator.scala
+++ src/main/scala/com/cars/ingestionframework/exampleapp/ExampleCustomActionCreator.scala
@@ -3,6 +3,10 @@ package com.cars.ingestionframework.exampleapp
 import org.apache.spark.sql.hive.HiveContext
 import org.json4s.JValue
 
+import com.cars.ingestionframework.ActionCreator
+import com.cars.ingestionframework.Action
+import com.cars.ingestionframework.AddEnrichedFields
+
 /** Example 
   */
 class ExampleCustomActionCreator extends ActionCreator {

75d1dc5d06cd0f29d1dcdfed178ea5eeb9d400dd
Michael Sesterhenn <invadergir@users.noreply.github.com>
2016-05-31 11:55:20 -0500

    Any jar version will get copied up.

--- scripts/copyjar-nonprod.sh
+++ scripts/copyjar-nonprod.sh
@@ -7,4 +7,4 @@ THISDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 set -x
 cd $THISDIR/..
 
-scp -p ./target/scala-2.10/ingestionframework_2.10-0.1.jar msesterh@cj4hdl001.cars.com:/tmp/spark_jar/
+scp -p $(find ./target/scala-2.10/ingestionframework*.jar) msesterh@cj4hdl001.cars.com:/tmp/spark_jar/

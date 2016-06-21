f97fd02eb613e6590c87ee64dfc9d69d4382888b
Michael Sesterhenn <invadergir@users.noreply.github.com>
2016-06-21 17:26:18 -0500

    Replace-null-with working again

--- src/main/scala/com/cars/turbocow/actions/ReplaceNullWith.scala
+++ src/main/scala/com/cars/turbocow/actions/ReplaceNullWith.scala
@@ -3,12 +3,30 @@ package com.cars.turbocow.actions
 import com.cars.turbocow.Action
 import com.cars.turbocow.ActionContext
 import com.cars.turbocow.Defs
+import com.cars.turbocow.JsonUtil
 import com.cars.turbocow.PerformResult
 import org.json4s._
 
 
-class ReplaceNullWith(replacement: Int, actionConfig: JValue) extends Action
+class ReplaceNullWith(replacement: String, sourceList: List[String]) extends Action
 {
+  /** Construct from a JValue config
+    */
+  def this(replacement: String, actionConfig: JValue) = {
+    this( 
+      replacement, 
+      actionConfig.toOption match {
+        case None => throw new Exception("'config' section is missing in 'replace-null-with' action.")
+        case Some(config) => {
+          val jSourceList = (actionConfig \ "source" ).children
+          val sourceList = jSourceList.flatMap( e=> JsonUtil.extractValidString(e) )
+          // check for any empty fields
+          if (sourceList.size != jSourceList.size) throw new Exception("'replace-null-with' actions must not have source fields that are null or empty.")
+          sourceList
+        }
+      }
+    )
+  }
 
   /** Replace a null value with something else.
     *
@@ -21,26 +39,19 @@ class ReplaceNullWith(replacement: Int, actionConfig: JValue) extends Action
 
     implicit val jsonFormats = org.json4s.DefaultFormats
 
-    // for each sourceField, get the data out of the inputRecord, and see if we need to replace it with a value
-    //PerformResult( 
-    //  sourceFields.flatMap{ field => 
-    //
-    //    // search in the source json for this field name.
-    //    val found = (inputRecord \ field)
-    //
-    //    // TODO these could be separated out into separate actions, but this is fine:
-    //    if(found == JNothing || found == JNull) {
-    //      // Add this as the value specified:
-    //      Some((field, replacement.toString))
-    //    }
-    //    else {
-    //      // Otherwise, just do a copy.
-    //      Some((field, found.extract[String]))
-    //    }
-    //    
-    //  }.toMap
-    //)
-    PerformResult(Defs.emptyStringMap)
+    // for each sourceField, get the data out of the inputRecord, and add it to map to return.
+    val enrichedUpdates = sourceList.flatMap{ inputFieldName =>
+    
+      // search in the source json for this field name.
+      val inputFieldValue = JsonUtil.extractOptionString(inputRecord \ inputFieldName)
+
+      inputFieldValue match {
+        case None => Some( (inputFieldName, replacement) )
+        case Some(fieldVal) => None
+      }
+    }.toMap
+    
+    PerformResult(enrichedUpdates)
   }
 }
 

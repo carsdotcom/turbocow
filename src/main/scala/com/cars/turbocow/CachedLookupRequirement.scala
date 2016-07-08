9b600ec2016d11d117938eb45ad97ae9f59a69bf
Michael Sesterhenn <invadergir@users.noreply.github.com>
2016-07-08 11:11:29 -0500

    Moved TableCache.getAllLookupRequirements() to CachedLookupRequirement.getAllFrom().

--- src/main/scala/com/cars/turbocow/CachedLookupRequirement.scala
+++ src/main/scala/com/cars/turbocow/CachedLookupRequirement.scala
@@ -26,3 +26,41 @@ case class CachedLookupRequirement(
   }
 
 }
+
+object CachedLookupRequirement {
+
+  /** Scan a list of Items and return a map of dbAndTable to list of 
+    * all Lookup actions on that table.
+    * 
+    * @param  items list to search through for actions needing cached lookup tables
+    * @return list of CachedLookupRequirements objects, one per table
+    */
+  def getAllFrom(items: List[Item]):
+    List[CachedLookupRequirement] = {
+
+    val allReqList: List[ (String, CachedLookupRequirement) ] = items.flatMap{ item =>
+      val actionReqs: List[CachedLookupRequirement] = item.actions.flatMap{ action =>
+        action.getLookupRequirements
+      }
+      actionReqs
+    }.map{ req => (req.dbTableName, req) }
+
+    val allReqsMap: Map[ String, List[CachedLookupRequirement]] = 
+      allReqList.groupBy{ _._1 }.map{ case(k, list) => (k, list.map{ _._2 } ) }
+
+    // combine to form one map of dbTableName to requirements.
+    // I feel like this last bit could be simplified.  TODO
+    val combinedRequirements: Map[String, CachedLookupRequirement] = allReqsMap.map{ case (dbTableName, reqList) =>
+      ( dbTableName,
+        reqList.reduceLeft{ (combined, e) =>
+          combined.combineWith(e)
+        } 
+      )
+    }
+
+    combinedRequirements.toList.map{ _._2 }
+  }
+
+
+}
+

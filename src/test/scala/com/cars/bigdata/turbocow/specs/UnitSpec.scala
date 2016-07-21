6d345438169f47b0a3c25879bd9a079693ae86a0
Michael Sesterhenn <invadergir@users.noreply.github.com>
2016-07-20 23:02:37 -0500

    Add mockito to UnitSpec.

--- src/test/scala/com/cars/bigdata/turbocow/specs/UnitSpec.scala
+++ src/test/scala/com/cars/bigdata/turbocow/specs/UnitSpec.scala
@@ -3,11 +3,13 @@ package com.cars.bigdata.turbocow
 import org.scalatest.{Matchers, FunSpec}
 
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
+import org.scalatest.mock.MockitoSugar
 
 abstract class UnitSpec 
   extends FunSpec 
   with Matchers 
   with BeforeAndAfterAll
   with BeforeAndAfterEach
+  with MockitoSugar
 
 

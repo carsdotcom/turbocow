325c0100977a5d120d8ef4319df8097d55c0dc8d
Michael Sesterhenn <invadergir@users.noreply.github.com>
2016-07-25 18:38:53 -0500

    Added implementation of fieldSource parameter of unary check

--- src/main/scala/com/cars/bigdata/turbocow/actions/checks/CheckParams.scala
+++ src/main/scala/com/cars/bigdata/turbocow/actions/checks/CheckParams.scala
@@ -1,11 +1,62 @@
 package com.cars.bigdata.turbocow.actions
 
+import com.cars.bigdata.turbocow.{FieldSource, JsonUtil}
+import org.json4s.JValue
+
 case class CheckParams(
   val left: String,
-  val right: Option[String] = None
-//  val leftSource: Option[String] = None,
-//  val rightSource: Option[String] = None
-) extends Serializable
+  val right: Option[String] = None,
+  val leftSource: Option[FieldSource.Value] = None,
+  val rightSource: Option[FieldSource.Value] = None
+) extends Serializable {
+
+  /** Get the value from either input record or enriched map.
+    */
+  def getValueFrom(
+    field: Option[String],
+    fieldSource: Option[FieldSource.Value],
+    inputRecord: JValue, 
+    currentEnrichedMap: Map[String, String]
+  ): Option[String] = {
+
+    val valueOpt = field match {
+      case None => None
+      case Some(f) => {
+        fieldSource match {
+          case None => { 
+            val enrichedOpt = currentEnrichedMap.get(f)
+            if (enrichedOpt.nonEmpty) enrichedOpt
+            else JsonUtil.extractOptionString(inputRecord \ f)
+          }
+          case Some(FieldSource.Input) => JsonUtil.extractOptionString(inputRecord \ f)
+          case Some(FieldSource.Enriched) => currentEnrichedMap.get(f)
+          case a: Any => throw new Exception("unrecognized field source:"+ a.toString)
+        }
+      }
+    }
+
+    // Special case:  enriched record could potentially have an actual null as the value.
+    // Check and convert to None.
+    if (valueOpt.nonEmpty && valueOpt.get == null ) None
+    else valueOpt
+  }
+
+  /** Get the left value from the correct place, or None if not exists.
+    */
+  def getLeftValue(
+    inputRecord: JValue, 
+    currentEnrichedMap: Map[String, String]): 
+    Option[String] = 
+    getValueFrom(Option(left), leftSource, inputRecord, currentEnrichedMap)
+
+  /** Get the right value from the correct place, or None if not exists.
+    */
+  def getRightValue(
+    inputRecord: JValue, 
+    currentEnrichedMap: Map[String, String]): 
+    Option[String] = 
+    getValueFrom(right, leftSource, inputRecord, currentEnrichedMap)
+}
 
 object CheckParams {
 
---
+++ src/test/scala/com/cars/bigdata/turbocow/specs/CheckParamsSpec.scala
@@ -0,0 +1,80 @@
+package com.cars.bigdata.turbocow
+
+import com.cars.bigdata.turbocow.actions._
+import org.json4s.jackson.JsonMethods._
+
+class CheckParamsSpec extends UnitSpec {
+
+  // before all tests have run
+  override def beforeAll() = {
+    super.beforeAll()
+  }
+
+  // before each test has run
+  override def beforeEach() = {
+    super.beforeEach()
+  }
+
+  // after each test has run
+  override def afterEach() = {
+    super.afterEach()
+  }
+
+  // after all tests have run
+  override def afterAll() = {
+    super.afterAll()
+  }
+
+  describe("getValueFrom()") {
+
+    it("should return the valid enriched value if specified") {
+      val cp = CheckParams(left = "A", leftSource = Option(FieldSource.Enriched ))
+      val input = parse("""{"A": "AInput"}""")
+      val enriched = Map("A"->"AEnriched")
+      cp.getValueFrom(Option(cp.left), cp.leftSource, input, enriched) should be (Some("AEnriched"))
+    }
+    it("should return None if enriched is specified but value not in enriched") {
+      val cp = CheckParams(left = "A", leftSource = Option(FieldSource.Enriched ))
+      val input = parse("""{"A": "AInput"}""")
+      val enriched = Map("XXX"->"XEnriched")
+      cp.getValueFrom(Option(cp.left), cp.leftSource, input, enriched) should be (None)
+    }
+
+    it("should return the valid input value if specified") {
+      val cp = CheckParams(left = "A", leftSource = Option(FieldSource.Input ))
+      val input = parse("""{"A": "AInput"}""")
+      val enriched = Map("A"->"AEnriched")
+      cp.getValueFrom(Option(cp.left), cp.leftSource, input, enriched) should be (Some("AInput"))
+    }
+    it("should return None if input is specified but value not in input") {
+      val cp = CheckParams(left = "A", leftSource = Option(FieldSource.Input ))
+      val input = parse("""{"XA": "XAInput"}""")
+      val enriched = Map("A"->"AEnriched")
+      cp.getValueFrom(Option(cp.left), cp.leftSource, input, enriched) should be (None)
+    }
+
+    it("by default, should return the enriched value if enriched has a value") {
+      val cp = CheckParams(left = "A")
+      val input = parse("""{"A": "AInput"}""")
+      val enriched = Map("A"->"AEnriched")
+      cp.getValueFrom(Option(cp.left), cp.leftSource, input, enriched) should be (Some("AEnriched"))
+    }
+    it("by default, should return the input value if enriched does not have the value but input does") {
+      val cp = CheckParams(left = "A")
+      val input = parse("""{"A": "AInput"}""")
+      val enriched = Map("XXA"->"XXAEnriched")
+      cp.getValueFrom(Option(cp.left), cp.leftSource, input, enriched) should be (Some("AInput"))
+    }
+    it("by default, should return None if neither enriched nor input has the value") {
+      val cp = CheckParams(left = "A")
+      val input = parse("""{"XXA": "XXXAInput"}""")
+      val enriched = Map("XXA"->"XXAEnriched")
+      cp.getValueFrom(Option(cp.left), cp.leftSource, input, enriched) should be (None)
+    }
+  }
+
+}
+
+
+
+

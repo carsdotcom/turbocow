72ee2953b579c5e4702735878668fe7e7d45ffed
nchaturvedula <nchaturvedula-contractor@cars.com>
2016-07-26 15:09:23 -0500

    changes made based on PR comments

--- src/main/scala/com/cars/bigdata/turbocow/actions/checks/NumericChecker.scala
+++ src/main/scala/com/cars/bigdata/turbocow/actions/checks/NumericChecker.scala
@@ -1,7 +1,7 @@
 package com.cars.bigdata.turbocow.actions.checks
 
 import com.cars.bigdata.turbocow.actions.{CheckParams, Checker}
-import com.cars.bigdata.turbocow.{ActionContext, JsonUtil, ValidString}
+import com.cars.bigdata.turbocow.{ActionContext, JsonUtil}
 import org.json4s.JValue
 import scala.util.{Try, Success, Failure}
 
@@ -19,13 +19,8 @@ class NumericChecker extends Checker {
     // get the test value
     val testVal = JsonUtil.extractValidString(inputRecord \ checkParams.left)
     if(testVal.isDefined) {
-      testVal.get.matches(s"""[+-]?((\\d+(e\\d+)?[lL]?)|(((\\d+(\\.\\d*)?)|(\\.\\d+))(e\\d+)?[fF]?))""")
-      /*//checking if the String is a number. gives success or failure accordingly
-        val result = Try(testVal.get.toDouble)
-        result match {
-          case Success(v) => true
-          case Failure(e) => false
-        }*/
+      //just making sure converting is sucessful without any exceptions and doesn't contain double float identifiers at the end
+      Try{testVal.get.toDouble}.isSuccess && !List( 'd', 'D', 'f', 'F').contains(testVal.get.trim.last)
     }
     else{
       false

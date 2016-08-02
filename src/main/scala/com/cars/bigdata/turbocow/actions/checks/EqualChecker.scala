602d12e54992fd66753ae7934403751bf6f83eba
Kranti Gorthy <kgorthy@cars.com>
2016-08-02 08:47:53 -0500

    BD-614| When sources are different for left and right

--- src/main/scala/com/cars/bigdata/turbocow/actions/checks/EqualChecker.scala
+++ src/main/scala/com/cars/bigdata/turbocow/actions/checks/EqualChecker.scala
@@ -1,6 +1,7 @@
 package com.cars.bigdata.turbocow.actions.checks
 
 import com.cars.bigdata.turbocow.{ActionContext, FieldSource, JsonUtil}
+import org.json4s.JsonAST.JValue
 import org.json4s.{JValue, JsonAST}
 
 class EqualChecker extends Checker {
@@ -14,11 +15,11 @@ class EqualChecker extends Checker {
                     context: ActionContext): Boolean = {
 
     val leftVal = checkParams.leftSource match {
-          case Some(FieldSource.Input) => inputRecord \ checkParams.left
-          case Some(FieldSource.Enriched) => currentEnrichedMap.get(checkParams.left)
-          case Some(FieldSource.Constant) => checkParams.left
-          case None => inputRecord \ checkParams.left
-          case a: Any => throw new Exception("unrecognized field source:"+ a.toString)
+      case Some(FieldSource.Input) => inputRecord \ checkParams.left
+      case Some(FieldSource.Enriched) => currentEnrichedMap.get(checkParams.left)
+      case Some(FieldSource.Constant) => checkParams.left
+      case None => inputRecord \ checkParams.left
+      case a: Any => throw new Exception("unrecognized field source:" + a.toString)
     }
 
     val rightOption: Option[String] = checkParams.right
@@ -29,18 +30,42 @@ class EqualChecker extends Checker {
     val rightVal = checkParams.rightSource match {
       case Some(FieldSource.Input) => inputRecord \ rightOption.get
       case Some(FieldSource.Enriched) => currentEnrichedMap.get(rightOption.get)
-      case Some(FieldSource.Constant) => rightOption.get
+      case Some(FieldSource.Constant) => rightOption
       case None => inputRecord \ rightOption.get
-      case a: Any => throw new Exception("unrecognized field source:"+ a.toString)
+      case a: Any => throw new Exception("unrecognized field source:" + a.toString)
     }
 
     if (leftVal.isInstanceOf[JsonAST.JNothing.type] && rightVal.isInstanceOf[JsonAST.JNothing.type]) {
       return false
     }
 
+    if (leftVal.isInstanceOf[JsonAST.JValue]) {
+      return handleLeftJValue(leftVal, rightVal)
+    } else if (leftVal.isInstanceOf[Option[String]]) {
+      return handleLeftOptionString(leftVal, rightVal)
+    }
+
     leftVal.equals(rightVal)
   }
 
+  def handleLeftOptionString(leftVal: Object, rightVal: Object): Boolean = {
+    if (rightVal.isInstanceOf[JValue.type]) {
+      val rightValidStringVal = JsonUtil.extractValidString(rightVal.asInstanceOf[JValue])
+      return leftVal.equals(rightValidStringVal)
+    }
+    return leftVal.equals(rightVal)
+  }
+
+  def handleLeftJValue(leftVal: Object, rightVal: Object): Boolean = {
+    val leftValidStringVal = JsonUtil.extractValidString(leftVal.asInstanceOf[JValue])
+    if (!rightVal.isInstanceOf[JValue.type]) {
+      return leftValidStringVal.equals(rightVal)
+    } else if (rightVal.isInstanceOf[JValue.type]) {
+      val rightValidStringVal = JsonUtil.extractValidString(rightVal.asInstanceOf[JValue])
+      return leftValidStringVal.equals(rightValidStringVal)
+    }
+    return false
+  }
 }
 
 

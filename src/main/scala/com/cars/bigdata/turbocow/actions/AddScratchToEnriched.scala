0dea7283b8f9229280b63ab2e2681d68ebb784f1
Ross Stuart <rstuart@cars.com>
2016-08-25 15:09:08 -0500

    add additinal tests

--- src/main/scala/com/cars/bigdata/turbocow/actions/AddScratchToEnriched.scala
+++ src/main/scala/com/cars/bigdata/turbocow/actions/AddScratchToEnriched.scala
@@ -2,25 +2,52 @@ package com.cars.bigdata.turbocow.actions
 
 import com.cars.bigdata.turbocow.{Action, ActionContext, JsonUtil, PerformResult}
 import org.json4s.JsonAST.JValue
+import org.json4s._
 
-class AddScratchToEnriched(keyArray: List[String]) extends Action
+class AddScratchToEnriched(val keys: List[String]) extends Action
 {
+  //Check keyArray inputs
+  if (keys.isEmpty) throw new Exception("keyArray is emptly")
+  for (key <- keys) {
+    if (key.trim.isEmpty) throw new Exception("keyArray fields are empty or blank strings")
+  }
+
+  /**
+    * Secondary JSON constructor
+    * @param actionConfig
+    */
   def this(actionConfig: JValue) = {
     this(
-      keyArray = (actionConfig \ "fields").children.map(eachKey => JsonUtil.extractString(eachKey))
+      actionConfig.toOption match {
+        case None => throw new Exception("'config' section is missing in 'AddScratchToEnriched' action.")
+        case Some(actionConfig) => {
+          val fields = (actionConfig \ "fields")
+          fields match {
+            case jA: JArray => ;
+            case _ => throw new Exception("Fields isn't a JArray")
+          }
+          if (fields.children.isEmpty) throw new Exception("fields array is empty")
+          val keys = (fields.children.map(eachKey => JsonUtil.extractOptionString(eachKey).get.trim))
+          for (key <- keys) {
+            if (key.isEmpty) throw new Exception("key in fields array is empty")
+          }
+          keys
+        }
+      }
     )
   }
+
   /** Perform the action
     */
   def perform(
-    inputRecord: JValue, 
+    inputRecord: JValue,
     currentEnrichedMap: Map[String, String],
     context: ActionContext):
     PerformResult = {
 
   // How do wrap in Option to avoid null checks on context.scratchPad.get("dateId")
   PerformResult(
-    keyArray.map { eachKey =>
+    keys.map { eachKey =>
       val getVal: Option[Any] = context.scratchPad.get(eachKey)
       if (getVal isDefined) {
         // Just add this to the enriched data - doesn't matter

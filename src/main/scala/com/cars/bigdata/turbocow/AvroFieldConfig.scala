e9ca288e37ab90943e190e7fb2dfa1abc974bf73
Michael Sesterhenn <invadergir@users.noreply.github.com>
2016-08-31 17:38:00 -0500

    Default values are working.

--- src/main/scala/com/cars/bigdata/turbocow/AvroFieldConfig.scala
+++ src/main/scala/com/cars/bigdata/turbocow/AvroFieldConfig.scala
@@ -1,13 +1,41 @@
 package com.cars.bigdata.turbocow
 
-import org.json4s.JValue
 import AvroOutputWriter._
-import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types._
+import org.json4s.JsonAST._
 
 case class AvroFieldConfig(
   structField: StructField,
   defaultValue: JValue
-)
+) {
+
+  /** Get the default value according to what type it is.
+    *  
+    * @return 'primitive' type (Int, Float, etc.) in an Any;
+    *         throws on JNothing (no default provided) or
+    *         the JSON type was not a string, numeric, boolean,
+    *         or null.
+    */
+  def getDefaultValue: Any = {
+    implicit val jsonFormats = org.json4s.DefaultFormats
+    defaultValue match {
+      case j: JString => j.extract[String]
+      case j: JInt => structField.dataType match {
+        case IntegerType => j.extract[Int]
+        case LongType => j.extract[Long]
+      }
+      case j: JDouble => structField.dataType match {
+        case FloatType => j.extract[Float]
+        case DoubleType => j.extract[Double]
+      }
+      case j: JBool => j.extract[Boolean]
+      case JNull => null
+      case JNothing => throw new Exception("no default value was specified")
+      case _ => throw new Exception(s"unsupported JSON type specified as 'default' value for '${structField.name}' field.")
+    }
+  }
+
+}
 
 object AvroFieldConfig {
 

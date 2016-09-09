34b754c98b9d928ee98684970251329747fa9da8
Michael Sesterhenn <invadergir@users.noreply.github.com>
2016-09-09 15:26:59 -0500

    Remove unused value.

--- src/main/scala/com/cars/bigdata/turbocow/AvroOutputWriterConfig.scala
+++ src/main/scala/com/cars/bigdata/turbocow/AvroOutputWriterConfig.scala
@@ -8,12 +8,7 @@ case class AvroOutputWriterConfig(
   // Without it, the conversion will fail but if you are very strict with your 
   // data this may be needed.
   alwaysTrimNumerics: Boolean = true,
-  alwaysTrimBooleans: Boolean = true,
-
-  // Treat empty trimmed numeric or boolean values as missing, and populate 
-  // with default values.  Strings are not defaulted if empty, that may be a valid 
-  // string.
-  treatEmptyValuesAsMissing: Boolean = true
+  alwaysTrimBooleans: Boolean = true
 )
 
 

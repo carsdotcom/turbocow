af5af016005b323e0c02749359f4d4d1b0b4ff9a
Michael Sesterhenn <invadergir@users.noreply.github.com>
2016-09-13 15:40:54 -0500

    Added AvroSchemaStringifier.writeStringSchema()

--- src/test/scala/com/cars/bigdata/turbocow/specs/AvroSchemaStringifierSpec.scala
+++ src/test/scala/com/cars/bigdata/turbocow/specs/AvroSchemaStringifierSpec.scala
@@ -6,11 +6,16 @@ import org.json4s.jackson.Serialization.{read, write}
 
 import scala.io.Source
 
+import utils.FileUtil
 
 class AvroSchemaStringifierSpec extends UnitSpec {
 
   implicit val jsonFormats = org.json4s.DefaultFormats
                     
+  def writeFile(text: String, filename: String) = {
+    FileUtil.writeFile(text, filename)
+  }
+
   // before all tests have run
   override def beforeAll() = {
     super.beforeAll()
@@ -38,54 +43,54 @@ class AvroSchemaStringifierSpec extends UnitSpec {
   //////////////////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////
 
+  val testAvroSchemaString = """{
+    "namespace": "ALS",
+    "type": "record",
+    "name": "impression",
+    "doc": "global documentation",
+    "fields": [{
+        "name": "StringField",
+        "type": [ "string" ],
+        "doc": "StringFieldDoc",
+        "default": "0"
+      }, {
+        "name": "IntField",
+        "type": [ "null", "int" ],
+        "doc": "IntFieldDoc",
+        "default": 1
+      }, {
+        "name": "LongField",
+        "type": [ "null", "long" ],
+        "default": 2
+      }, {
+        "name": "DoubleField",
+        "type": [ "double" ],
+        "default": 0.3
+      }, {
+        "name": "FloatField",
+        "type": [ "null", "float" ],
+        "default": 0.4
+      }, {
+        "name": "BooleanField",
+        "type": [ "null", "boolean" ],
+        "default": false
+      }, {
+        "name": "NullField",
+        "type": [ "null" ],
+        "default": null
+      }, {
+        "name": "LongNullDefault",
+        "type": [ "null", "long" ],
+        "default": null
+      }, {
+        "name": "StringNullDefault",
+        "type": [ "null", "string" ],
+        "default": null
+      }
+    ]
+  }"""
+
   describe("convertToStringTypes()") {
-    
-    val testAvroSchemaString = """{
-      "namespace": "ALS",
-      "type": "record",
-      "name": "impression",
-      "doc": "global documentation",
-      "fields": [{
-          "name": "StringField",
-          "type": [ "string" ],
-          "doc": "StringFieldDoc",
-          "default": "0"
-        }, {
-          "name": "IntField",
-          "type": [ "null", "int" ],
-          "doc": "IntFieldDoc",
-          "default": 1
-        }, {
-          "name": "LongField",
-          "type": [ "null", "long" ],
-          "default": 2
-        }, {
-          "name": "DoubleField",
-          "type": [ "double" ],
-          "default": 0.3
-        }, {
-          "name": "FloatField",
-          "type": [ "null", "float" ],
-          "default": 0.4
-        }, {
-          "name": "BooleanField",
-          "type": [ "null", "boolean" ],
-          "default": false
-        }, {
-          "name": "NullField",
-          "type": [ "null" ],
-          "default": null
-        }, {
-          "name": "LongNullDefault",
-          "type": [ "null", "long" ],
-          "default": null
-        }, {
-          "name": "StringNullDefault",
-          "type": [ "null", "string" ],
-          "default": null
-        }
-      ]
-    }"""
     
     it("should convert an avro schema (as JSON string) to all-string types") {
       val modJson = AvroSchemaStringifier.convertToStringTypes(testAvroSchemaString)
@@ -112,7 +117,34 @@ class AvroSchemaStringifierSpec extends UnitSpec {
   describe("writeStringSchema()") {
 
     it("should convert an avro schema JSON file to all-string types and write to the dir specified") {
-      fail()
+      import java.nio.file.Files
+      val tempDir = Files.createTempDirectory("test-AvroSchemaStringifier-")
+      val origSchemaFile = s"$tempDir/origSchema"
+      val outputSchemaFile = s"$tempDir/outputSchema"
+
+      writeFile(testAvroSchemaString, origSchemaFile)
+
+      AvroSchemaStringifier.writeStringSchema(origSchemaFile, outputSchemaFile)
+
+      import scala.io.Source
+      val outputStr = Source.fromFile(outputSchemaFile).getLines.mkString
+
+      val a = AvroSchema(outputStr)
+
+      a.namespace should be ("ALS")
+      a.`type` should be ("record")
+      a.name should be ("impression")
+      a.doc should be ("global documentation")
+      a.fields.size should be (9)
+      a.fields(0) should be (AvroSchemaField("StringField",       List("string"),         JString("0"), "StringFieldDoc"))
+      a.fields(1) should be (AvroSchemaField("IntField",          List("null", "string"), JString("1"), "IntFieldDoc"))
+      a.fields(2) should be (AvroSchemaField("LongField",         List("null", "string"), JString("2"), ""))
+      a.fields(3) should be (AvroSchemaField("DoubleField",       List(        "string"), JString("0.3"), ""))
+      a.fields(4) should be (AvroSchemaField("FloatField",        List("null", "string"), JString("0.4"), ""))
+      a.fields(5) should be (AvroSchemaField("BooleanField",      List("null", "string"), JString("false"), ""))
+      a.fields(6) should be (AvroSchemaField("NullField",         List("null"          ), JNull, ""))
+      a.fields(7) should be (AvroSchemaField("LongNullDefault",   List("null", "string"), JNull, ""))
+      a.fields(8) should be (AvroSchemaField("StringNullDefault", List("null", "string"), JNull, ""))
     }
   }
 
